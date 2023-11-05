d612e5db7359eb530e382d85eff375682de94f82
Kenny <101211455+thatDepressedCoder@users.noreply.github.com>
2023-11-05 00:35:47 -0400

    Timer work

--- resources/script.js
+++ resources/script.js
@@ -1,15 +1,25 @@
-const startingMinutes = 60;
+const startingMinutes = 60; // 60 minutes
 let time = startingMinutes * 60;
 
 const countdownEl = document.getElementById('countdown');
-
-setInterval(updateCountdown, 1000);
 
 function updateCountdown() {
     const minutes = Math.floor(time / 60);
     let seconds = time % 60;
 
-    seconds = seconds < 10 ? '0' + seconds : seconds;
-    countdownEl.innerHTML = `$(minutes}: $(seconds}`;
-    time--;
+    const displaySeconds = seconds < 10 ? '0' + seconds : seconds;
+    countdownEl.innerHTML = `${minutes}:${displaySeconds}`;
+
+    if (time > 0) {
+        time--;
+    } else {
+        clearInterval(timer); // Stop the timer when it reaches 0
+    }
 }
+
+// Initial call to update the countdown immediately
+updateCountdown();
+
+// Start the countdown interval to update the timer every second
+const timer = setInterval(updateCountdown, 1000);
+
